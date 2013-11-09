832e9431101ed3e72ca5712346e065b7a6bf50d9
DrDyne <rchaumard@gmail.com>
2013-11-08 19:09:42 -0500

    yo backbone-amd

---
+++ app/scripts/main.js
@@ -0,0 +1,34 @@
+/*global require*/
+'use strict';
+
+require.config({
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        }
+    },
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        backbone: '../bower_components/backbone/backbone',
+        underscore: '../bower_components/underscore/underscore',
+        bootstrap: 'vendor/bootstrap'
+    }
+});
+
+require([
+    'backbone'
+], function (Backbone) {
+    Backbone.history.start();
+    console.log('Hello from Backbone!');
+});
