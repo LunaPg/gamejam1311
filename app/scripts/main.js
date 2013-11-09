a1afd460a3de7cf1a7b8b33584ecd59dff294bae
DrDyne <rchaumard@gmail.com>
2013-11-09 17:22:22 -0500

    created dependencies for models and views

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,20 +28,25 @@ require.config({
 });
 
 require([
-    'backbone',
-    'models/Element',
-    'handlebars',
-], function (Backbone, Element) {
-    Backbone.history.start();
-    console.log('Hello from Backbone!');
-    var element = new Element();
-    var elementCollection = Backbone.Collection.extend({
-      model: Element
-    });
-    var myElementCollection = new elementCollection();
+  'backbone',
+  'collections/Elements',
+  'models/index',
+  'views/GameEngine',
+  'resources/index',
+  'handlebars',
+], function (
+    Backbone,
+    Elements,
+    Models,
+    Game, 
+    Resources
+) {
+  console.log('Hello TAG2013!');
+  var Elements = new Elements(Resources.elements);
 
-    myElementCollection.add({});
-    myElementCollection.add({ name: "manamour" });
-console.log(myElementCollection);
-console.log(element);
+  var Achievements = new Views.Achievements({collection: Resources.achievements});
+  var Recipes = new Views.Recipes({collection: Resources.recipes});
+
+  var Game = new Views.GameEngine({ collection: Elements });
+  Backbone.history.start();
 });
