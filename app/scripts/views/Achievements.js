badd11a1bd64d7d65b0fe326a344e0bfb5b951b4
lunapg <malalunapg@gmail.com>
2013-11-10 11:41:52 -0500

    added Achievements model and collection ans template

--- app/scripts/views/Achievements.js
+++ app/scripts/views/Achievements.js
@@ -1,4 +1,34 @@
-define(['backbone', 'jquery'], function (Backbone, $) {
+define([
+	'backbone', 
+	'collections/index',
+  'resources/achievements',
+	'jquery',
+  'text!/templates/achievements.hbs'
+], function (Backbone, Collections, Achievements, $, tpl) {
   return Backbone.View.extend({
+    el: '.achievements-container',
+    template: Handlebars.compile(tpl),
+    initialize: function (options) {
+      this.collection = new Collections.Achievements(Achievements);
+    },
+    visible: false,
+    toggle: function() {
+      if (this.visible) {
+        this.$el.html('');
+        this.stopListening();
+      } else {
+			this.render()
+			};
+      this.visible = !this.visible;
+    },
+    render: function () {
+      var json = {
+        achievements : this.collection.map(function (item) { return item.serializeData() })
+      };
+      console.log(json);
+      this.$el.html(this.template(json));
+      return this;
+    },
+
   });
 });
