03d9e5adfaa313f21921ccb72608c843f703c65f
lunapg <malalunapg@gmail.com>
2013-11-25 16:55:28 -0500

    add element image file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -63,7 +63,10 @@ require([
 ) {
   console.log('TAG2013!');
   var Elements = new Elements(Resources.elements);
-
+	Elements.each(function(item){
+		item.set('status', 'unlocked');
+		item.set('count', 5);
+			})
   //var Achievements = new Views.Achievements({collection: Resources.achievements});
   //var Recipes = new Views.Recipes({collection: Resources.recipes});
 
